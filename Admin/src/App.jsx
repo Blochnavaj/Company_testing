acb6e3945905ba60d0af4af2fe53750c35467c8f
navaj bloch <navajbloch420@gmail.com>
2025-03-01 15:47:43 +0530

    change admin list name

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 
 import Add from "./Pages/Add";
-import LIst from "./Pages/LIst";
+import List from './Pages/List'
 import Orders from "./Pages/Orders";
 import Navbar from "./Components/Navbar";
 import SlideBar from "./Components/SlideBar";
@@ -32,7 +32,7 @@ function App() {
               <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-500 text-base">
                 <Routes>
                   <Route path="/add" element={<Add token={token} />} />
-                  <Route path="/list" element={<LIst token={token} />} />
+                  <Route path="/list" element={<List token={token} />} />
                   <Route path="/orders" element={<Orders token={token} />} />
                 </Routes>
               </div>
