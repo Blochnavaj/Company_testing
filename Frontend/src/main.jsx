27e836387446e9b8e65b14b36647671f3dca034b
navaj bloch <navajbloch420@gmail.com>
2025-03-01 15:36:01 +0530

    change connection url2

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
- import "./i18n.js"; // Import i18n
 import ShopContextProvider from './Context/ShopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
